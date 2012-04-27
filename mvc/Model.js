361b72efdf965d7d4e39d7329a505f8f0dd15242
Timothy Chandler <tim.chandler@spinifexgroup.com>
2012-04-27 15:07:17 +1000

    Added model locking and cloning.

--- mvc/Model.js
+++ mvc/Model.js
@@ -2,42 +2,49 @@
  * @class framework.mvc.Model
  * 
  * 
- * 
- * @mixins framework.trait.ComponentConnector
- * @mixins framework.trait.signal.Send
  * @abstract
  * 
- * @uses framework.trait.ComponentConnector
- * @uses framework.trait.signal.Send
  */
 $JSKK.Class.create
 (
 	{
 		$namespace:	'framework.mvc',
-		$name:		'Model',
-		$uses:
-		[
-//			framework.trait.ComponentConnector,
-			framework.trait.signal.Send
-		]
+		$name:		'Model'
 	}
 )
 (
-	{},
 	{
-		dirty:	false,
-		fields:	[],
-		record:	{},
-		store:	null,
+		LOCK_NONE:		'none',
+		LOCK_READONLY:	'readonly',
+		LOCK_FULL:		'full'
+	},
+	{
+		dirty:		false,
+		phantom:	false,
+		fields:		[],
+		record:		{},
+		store:		null,
+		/**
+		 * 
+		 * @property {String} lockState This property will block behaviours on this store depending on its state.
+		 * @private
+		 */
+		lockState:	'none',
+		/**
+		 * 
+		 */
 		init: function(record)
 		{
 			if (Object.isDefined(record))
 			{
+				this.record=Object.clone(this.fields);
 				for (var field in this.fields)
 				{
-					this.record[field]=this.fields[field];
+					if (Object.isDefined(record[field]))
+					{
+						this.record[field]=record[field];
+					}
 				}
-				this.record=record;
 			}
 			else
 			{
@@ -47,16 +54,25 @@ $JSKK.Class.create
 				}
 			}
 		},
+		/**
+		 * 
+		 */
 		bindStore: function(store)
 		{
 			this.store=store;
 			return this;
 		},
+		/**
+		 * 
+		 */
 		unbindStore: function()
 		{
 			this.store=null;
 			return this;
 		},
+		/**
+		 * 
+		 */
 		getStore: function()
 		{
 			return this.store;
@@ -79,25 +95,89 @@ $JSKK.Class.create
 		{
 			this.record[key]=value;
 			this.flagDirty();
-//			this.sendSignal(framework.Signal.MODEL_DONE_CHANGE,{id:this.getID()});
+//			this.sendSignal(framework.Signal.MODEL_DONE_CHANGE,{id:this.getStore().getID()});
 //			this.sendSignal('view.change',data);
 		},
+		/**
+		 * 
+		 */
 		flagDirty: function()
 		{
 			this.dirty=true;
 			return this;
 		},
+		/**
+		 * 
+		 */
 		flagClean: function()
 		{
 			this.dirty=false;
 		},
+		/**
+		 * 
+		 */
 		isDirty: function()
 		{
 			return this.dirty;
 		},
+		/**
+		 * 
+		 */
 		isClean: function()
 		{
 			return !this.dirty;
+		},
+		/**
+		 * Locks the model based on the type of lock given to this method.
+		 * @param {String} lockType The type of lock. Valid lock types are:
+		 * * {@link framework.mvc.Model#LOCK_NONE}
+		 * * {@link framework.mvc.Model#LOCK_READONLY}
+		 * * {@link framework.mvc.Model#LOCK_FULL}
+		 * 
+		 * @retrun {framework.data.stateful.Store}
+		 */
+		lock: function(lockType)
+		{
+			if (['none','readonly','full'].inArray(lockType))
+			{
+				this.lockState=lockType;
+				this.store.sendSignal
+				(
+					framework.Signal.MODEL_LOCK_CHANGE,
+					{
+						id:			this.getStore().getID(),
+						component:	this.getStore().getCmpName(),
+						model:		this,
+						lock:		this.lockState
+					}
+				);
+			}
+			else
+			{
+				throw new Error('"'+lockType+'" is an invalid lock type. Valid locks are "none", "readonly" and "full".');
+			}
+			return this;
+		},
+		/**
+		 * 
+		 */
+		clone: function()
+		{
+			return new (this.$reflect('self'))(this.record);
+		},
+		/**
+		 * 
+		 */
+		setPhantom: function(phantom)
+		{
+			this.phantom=Boolean(phantom);
+		},
+		/**
+		 * 
+		 */
+		isPhantom: function()
+		{
+			return this.phantom;
 		}
 	}
 );
