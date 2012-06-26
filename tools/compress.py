57238e9492f16006ffa75a9ade2514779924b957
Timothy Chandler <tim.chandler@spinifexgroup.com>
2012-06-26 14:36:12 +1000

    This should have gone with the previous commit...

--- tools/compress.py
+++ tools/compress.py
@@ -1,40 +1,57 @@
 import sys
 import os.path
+import thread
+import subprocess
 
 """ Some Config """
 
-DIR_STRAPPY='../';
-
+DIR_STRAPPY		='..\\'
+IGNORE_FOLDERS	=['.git','docs','tools']
 
 """ Main Compression Class """
 
 class Compress:
 	Name="Compress"
 	
+	
+	
 	def compressStrappy(self):
 		print 'locating files...'
-		#print os.path.exists(DIR_STRAPPY)
-		#print os.path.isdir(DIR_STRAPPY)
-		#print os.path.isfile(DIR_STRAPPY)
-		#print os.path.isabs(DIR_STRAPPY)
-
-		def callback( arg, dirname, fnames ):
-			sum = 0
-			#print dirname
-			#print fnames
-			for file in fnames:
-				print os.path.join(dirname, file)
-				sum += os.path.getsize(os.path.join(dirname, file))
-			arg.append(sum)
-			
-		arglist = []
-		os.path.walk(DIR_STRAPPY,callback,arglist)
 		
-		sum = 0
-		for value in arglist:
-			sum += value
-			
-		print "Size of directory:",sum
+		filelist	=[]
+		workingPath	=os.path.abspath(DIR_STRAPPY)
+		
+		def callback( arg, dirname, fnames ):
+			for file in fnames:
+				thisFile	=os.path.join(dirname, file)
+				fileParts	=os.path.splitext(thisFile)
+				dirParts	=fileParts[0].split('\\')
+				doContinue	=False
+				for dir in dirParts:
+					if dir in IGNORE_FOLDERS:
+						doContinue=True
+						break
+				
+				if doContinue:
+					doContinue=False
+					continue;
+				
+				if (fileParts[1]=='.js'):
+					arg.append(thisFile)
+		
+		os.path.walk(workingPath,callback,filelist)
+		
+		#print filelist
+		scripts='--js="'+'" --js="'.join(filelist)+'"'
+		
+		
+		result=subprocess.call(
+			'java '+
+			'-jar '+
+			'"'+os.path.join(os.path.realpath('.'),'compiler.jar')+'" '+
+			scripts
+		)
+		print result;
 
 
 
@@ -45,6 +62,17 @@ if (len(sys.argv) > 1):
 		print 'Compressing strappy...';
 		compressor=Compress();
 		compressor.compressStrappy();
+	elif(sys.argv[1]=='exec'):
+		print "Testing exec"
+		print 
+		
+		result=subprocess.call(
+			'java '+
+			'-jar '+
+			'"'+os.path.join(os.path.realpath('.'),'compiler.jar')+'" -?'
+		)
+		
+		print result
 	else:
 		print 'Invalid command.'
 		
