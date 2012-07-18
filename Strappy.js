51d855f211f268bf4f8d7f935f1f3afb159b138a
Timothy Chandler <tim.chandler@spinifexgroup.com>
2012-07-18 10:40:09 +1000

    Updated version number.

--- Strappy.js
+++ Strappy.js
@@ -1,12 +1,17 @@
 $JSKK.Class.create('Strappy')
 (
 	{
-		VERSION:				'1.0.0-beta',
+		VERSION:				'1.1.0-dev',
 		VERSION_MAJOR:			1,
-		VERSION_MINOR:			0,
+		VERSION_MINOR:			1,
 		VERSION_MICRO:			0,
-		VERSION_STAGE:			'beta',
-		VERSION_STAGE_NUM:		0
+		VERSION_STAGE:			'dev',
+		VERSION_STAGE_NUM:		0,
+		
+		config:
+		{
+			autoInitStateMgr:	false
+		}
 	},
 	{}
 );
