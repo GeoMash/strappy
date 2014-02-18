236f09004c2ea1354ba4c2af989d347fbc3f9197
Timothy Chandler <tim@praxisbt.com>
2014-02-18 16:17:37 +0800

    Updated version number.

--- src/Strappy.js
+++ src/Strappy.js
@@ -1,11 +1,11 @@
 $JSKK.Class.create('Strappy')
 (
 	{
-		VERSION:				'1.2.0-dev',
+		VERSION:				'1.2.0',
 		VERSION_MAJOR:			1,
 		VERSION_MINOR:			2,
 		VERSION_MICRO:			0,
-		VERSION_STAGE:			'dev',
+		VERSION_STAGE:			'',
 		VERSION_STAGE_NUM:		0,
 		
 		config:
