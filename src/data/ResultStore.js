046d4bf516cafb66203f7534d042a8e0eb4778c5
Timothy Chandler <tim@praxisbt.com>
2014-03-16 23:54:02 +0800

    Stub for result store.

---
+++ src/data/ResultStore.js
@@ -0,0 +1,23 @@
+/**
+ * @class strappy.data.MultiModelStore
+ * @extends strappy.data.AbstractStore
+ * 
+ * 
+ * @abstract
+ * 
+ */
+$JSKK.Class.create
+(
+	{
+		$namespace:		'strappy.data',
+		$name:			'ResultStore',
+		$extends:		'strappy.data.AbstractStore',
+		$abstract:		true
+	}
+)
+(
+	{},
+	{
+		
+	}
+);
